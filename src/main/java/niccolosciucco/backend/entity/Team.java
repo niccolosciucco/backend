e4f020431484ad4f52dd537024fe43b4b7c579e1
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-12 17:21:49 +0200

    Update Team.java

--- src/main/java/niccolosciucco/backend/entity/Team.java
+++ src/main/java/niccolosciucco/backend/entity/Team.java
@@ -3,6 +3,8 @@ package niccolosciucco.backend.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "teams")
 @Getter
@@ -12,12 +14,17 @@ import lombok.*;
 @Builder
 public class Team {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
+    @Column(name = "base", nullable = false)
     private String base;
+    @Column(name = "principal", nullable = false)
     private String principal;
+    @Column(name = "founded_year", nullable = false)
     private Integer foundedYear;
+    @Column(name = "color_hex", nullable = false, length = 7)
     private String colorHex;
 }
