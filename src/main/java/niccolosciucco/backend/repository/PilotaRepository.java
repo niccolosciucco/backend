4db39d3402670142bcdf86baa22356cf5bcb9137
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-12 17:37:22 +0200

    Create PilotaRepository.java

---
+++ src/main/java/niccolosciucco/backend/repository/PilotaRepository.java
@@ -0,0 +1,11 @@
+package niccolosciucco.backend.repository;
+
+import niccolosciucco.backend.entity.Pilota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PilotaRepository extends JpaRepository<Pilota, UUID> {
+    List<Pilota> findByTeamId(UUID teamId);
+}
