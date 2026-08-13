1c70808e5863e5e2ed2a533f55c7c5de88020610
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-13 11:49:06 +0200

    Update PilotaController.java

--- src/main/java/niccolosciucco/backend/controller/PilotaController.java
+++ src/main/java/niccolosciucco/backend/controller/PilotaController.java
@@ -2,7 +2,7 @@ package niccolosciucco.backend.controller;
 
 import lombok.RequiredArgsConstructor;
 import niccolosciucco.backend.entity.Pilota;
-import niccolosciucco.backend.repository.PilotaRepository;
+import niccolosciucco.backend.service.PilotaService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +14,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PilotaController {
 
-    private final PilotaRepository pilotaRepository;
+    private final PilotaService pilotaService;
 
     @GetMapping
     public List<Pilota> getAll() {
-        return pilotaRepository.findAll();
+        return pilotaService.getAll();
     }
 }
