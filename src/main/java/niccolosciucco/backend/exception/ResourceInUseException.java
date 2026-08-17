547667f17aeb5875f2c16b23282d9631a6d8b417
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-17 18:33:36 +0200

    Create ResourceInUseException.java

---
+++ src/main/java/niccolosciucco/backend/exception/ResourceInUseException.java
@@ -0,0 +1,7 @@
+package niccolosciucco.backend.exception;
+
+public class ResourceInUseException extends RuntimeException {
+    public ResourceInUseException(String message) {
+        super(message);
+    }
+}
