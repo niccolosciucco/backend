f2953b4034af2d87bbf2773286b704f9b0802be1
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-17 18:38:13 +0200

    Update PilotaRepository.java

--- src/main/java/niccolosciucco/backend/repository/PilotaRepository.java
+++ src/main/java/niccolosciucco/backend/repository/PilotaRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 
 public interface PilotaRepository extends JpaRepository<Pilota, UUID> {
     List<Pilota> findByTeamId(UUID teamId);
+
+    boolean existsByTeamId(UUID teamId);
 }
