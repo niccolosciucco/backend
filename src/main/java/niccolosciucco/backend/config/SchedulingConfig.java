9942e300f1574243d3ac9324ec73f04e0aa38134
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-18 12:46:04 +0200

    Create SchedulingConfig.java

---
+++ src/main/java/niccolosciucco/backend/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package niccolosciucco.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
