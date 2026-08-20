c7fdd21f49e6646195bbf3c6673443db3c69b19b
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-20 14:07:45 +0200

    Update SecurityConfig.java

--- src/main/java/niccolosciucco/backend/security/SecurityConfig.java
+++ src/main/java/niccolosciucco/backend/security/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/ws/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
