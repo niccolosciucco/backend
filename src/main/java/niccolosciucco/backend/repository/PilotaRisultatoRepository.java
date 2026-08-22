12cd7ea86ab34067315c3bbe724f1ea368c2f446
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-22 13:29:49 +0200

    Update PilotaRisultatoRepository.java

--- src/main/java/niccolosciucco/backend/repository/PilotaRisultatoRepository.java
+++ src/main/java/niccolosciucco/backend/repository/PilotaRisultatoRepository.java
@@ -3,7 +3,9 @@ package niccolosciucco.backend.repository;
 import niccolosciucco.backend.entity.PilotaRisultato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PilotaRisultatoRepository extends JpaRepository<PilotaRisultato, UUID> {
+    List<PilotaRisultato> findByPilotaId(UUID pilotaId);
 }
